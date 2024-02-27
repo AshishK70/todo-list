f37ceee6b101c86a1c7b638a6187a511a2622cc5
Ashish-72Dragons <Ashish@72Dragons.com>
2024-02-27 16:42:42 +0530

    Simple Todo App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Brush", quantity: 6, packed: true },
-  { id: 6, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Brush", quantity: 6, packed: true },
+//   { id: 6, description: "Charger", quantity: 1, packed: false },
+// ];
 
 
 function App() {
+  const [items,setItems] = useState([]);
+  function handleAddItems(item){
+    setItems(items=>[...items, item]);
+  }
+
+  function handleDeleteItem(id){
+    console.log("delete items with this ID : ",id)
+    setItems(items=>items.filter(item=>item.id !== id));
+  }
+
+  function toggleItem(id){
+    setItems((items) =>
+        items.map((item) =>
+          item.id === id ? { ...item, packed: !item.packed }:item)
+    );
+  }
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem = {handleDeleteItem} toggleItem={toggleItem}/>
       <Stats />
     </div>
   )
@@ -27,9 +44,12 @@ function Logo() {
   )
 }
 
-function Form() {
+function Form( {onAddItems}) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
+  // const [items,setItems] = useState([]);
+
+
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -44,6 +64,7 @@ function Form() {
       id: Date.now(),
     }
     console.log(newItem);
+    onAddItems(newItem)
     // reset the description and quanity 
     setDescription('');
     setQuantity(1);
@@ -53,7 +74,7 @@ function Form() {
       <label>Pack your items: </label>
       <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {/* getting the options dynamically with array.form and map meethod */}
-        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+        {Array.from({ length: 10 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
         {/* <option>Select Opion</option>
         <option value={1}>1</option>
         <option value={2}>2</option>
@@ -65,21 +86,22 @@ function Form() {
   )
 }
 
-function PackingList() {
+function PackingList({items, onDeleteItem, toggleItem}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map(item => <Item item={item} key={item.id} />)}
+        {items.map(item => <Item item={item} onDeleteItem={onDeleteItem} key={item.id} toggleItem={toggleItem}/>)}
       </ul>
     </div>
   )
 }
 
-function Item({ item }) {
+function Item({ item , onDeleteItem, toggleItem}) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={()=>toggleItem(item.id)} />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}} >{item.quantity} {item.description}</span>
-      <button>❌</button>
+      <button onClick={()=>onDeleteItem(item.id)}>❌</button>
     </li>
   )
 }
